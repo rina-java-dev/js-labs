04bc88b4a4a265ee3d7c102219edb53c61bcc454
marinagumeniuc <marina.gumeniuk.ani@gmail.com>
2026-01-27 16:57:51 +0200

    refactored structure

---
+++ Lab1/script.js
@@ -0,0 +1,20 @@
+let name = 'Marina Gumeniuc';
+let birthYear = 2004;
+let isStudent = true; // or 1
+
+console.log("Name: " + name);
+console.log("Birth Year: " + birthYear);
+console.log("Is Student: " + isStudent);
+
+let score = prompt("Введите ваш балл:");
+if (score >= 90) {
+  console.log("Отлично!");
+} else if (score >= 70) {
+  console.log("Хорошо");
+} else {
+  console.log("Можно лучше!");
+}
+
+for (let i = 1; i <= 5; i++) {
+  console.log(`Итерация: ${i}`);
+}
